752a90e968a382a1db43ea409fec5c41ed8293a4
andrewsixsixsix <a.laptiiev13@gmail.com>
2024-08-03 13:52:43 +0300

    Initial Commit

---
+++ BusyDay/BusyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusyDayApp.swift
+//  BusyDay
+//
+//  Created by Andrii Laptiiev on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BusyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
